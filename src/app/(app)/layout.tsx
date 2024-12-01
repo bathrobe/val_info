6df4fb8956521d47b0e9a2e4825ee3a3cd19fd2b
Joe <joe.holmes87@gmail.com>
2024-12-01 18:54:03 -0500

    redo

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-slate-900 text-blue-50">
+      <body className="bg-zinc-900 text-blue-50">
         <main>{children}</main>
       </body>
     </html>
